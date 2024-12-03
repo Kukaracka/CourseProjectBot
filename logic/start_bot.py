0b0b02559f8cdefea6df013d3cd9bb3d384dceb0
Michael <kukaracka@inbox.ru>
2024-12-04 05:21:11 +0700

    Усовершенствован парсер, рефакторинг кода и добавлена проверка входных данных

--- logic/start_bot.py
+++ logic/start_bot.py
@@ -1,32 +1,39 @@
+import sys
+import os
+from pprint import pprint
+sys.path.append(os.path.join(os.getcwd(), 'logic'))
+pprint(sys.path)
+
+
 import asyncio
 import logging
-from time import sleep, time
 import datetime as dt
 import statistics
-from creating_functions import *
-import keyboards as kb
+from logic.chatting_functions import *
+import logic.keyboards as kb
 
-from aiogram import Bot, Dispatcher
+
+
+from aiogram import Dispatcher
 from database import requests as rq
 from aiogram.types import FSInputFile
 
-from config.BOT_TOKEN import token
 
-from image_creation import create_image
+from logic.image_creation import create_image
 
-from handlers import router
-from bot import bot
+from logic.handlers import router
+from logic.bot import bot
 
 from database.models import async_main
 
 
 async def start_bot():
     await async_main()
-    asyncio.get_event_loop().create_task(check_time())
+    asyncio.get_event_loop().create_task(send_timely_messages())
     await dp.start_polling(bot)
 
 
-async def check_time():
+async def send_timely_messages():
     while True:
         # await asyncio.sleep(60)
         hours, minutes = dt.datetime.now().time().hour, dt.datetime.now().time().minute
@@ -44,8 +51,6 @@ async def check_time():
                 weather_temp_time.append(int(i))
             weather_temp_time = round(statistics.mean(weather_temp_time))
             weather_cond_time = weather_cond.split(",")[2]
-            day = dt.datetime.now().day
-            month = dt.datetime.now().month
             await create_image(style = 2, ratio="2:3", tg_id=current_user,
                                weather_mean=weather_temp_time, weather_cond=weather_cond_time)
 
@@ -54,23 +59,20 @@ async def check_time():
             weather_mess = await create_weather_list(weather_temp, weather_cond)
             image = FSInputFile(f"../images/{current_user}.jpg")
 
-            await bot.send_photo(chat_id=current_user, photo=image,
+            msg = await bot.send_photo(chat_id=current_user, photo=image,
                                  caption=f"Добрый день, {name} ❤️\n\n"
-                                         f"Погода на сегодня:\n\n"
-                                         f"{weather_mess}"f"Ваши задачи на день:\n\n"
+                                         f"Погода на сегодня:\n"
+                                         f"{weather_mess}"f"Ваши задачи на день:\n"
                                          f"{tasks}",
                                  reply_markup=kb.main_menu)
             await delete_messages(current_user)
+            await rq.setter_message_for_delete(current_user, msg.message_id)
         await asyncio.sleep(60)
 
 async def delete_messages(tg_id):
     messages_list = await rq.getter_messages_for_delete(tg_id)
     for tg_id, current_message in messages_list:
         await bot.delete_message(chat_id=tg_id, message_id=current_message)
-
-async def send_message(hours, minutes):
-    # await bot.send_message(chat_id=470068887, text=f"{hours} {minutes}")
-    pass
 
 
 if __name__ == '__main__':
